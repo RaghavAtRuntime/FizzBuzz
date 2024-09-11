5f0156034ecc0324b6e642b9ad78b60eb71b0a36
RaghavAtRuntime <sinharaghav291004@gmail.com>
2024-09-11 14:40:18 -0400

    Refactored to add Multiples.java

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class Multiples {
+    public static void main(String[] args) {
+        int n = 1000;
+        int k = 0;
+        for(int i = 0; i < n-1; i++) {
+            if(i % 3 == 0 || i % 5 == 0) {
+                k++;
+            }
+        }
+        System.out.println(k);
+    }
 }
